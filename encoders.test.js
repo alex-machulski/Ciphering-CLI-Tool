a734595712adfd4ccf5df61365130183ad4662ab
ICX\aliaksandr.machulski <aliaksandr.machulski@gmail.com>
2021-11-20 21:16:05 +0300

    feat: add tests for atbash cipher, small refactoring

--- encoders.test.js
+++ encoders.test.js
@@ -14,9 +14,15 @@ describe('caesar cipher', () => {
     })
 
     test('should correct encode english letters', () => {
-        const text = 'abcd';
+        const text = 'zAbCdaZ1';
         const caesarFlag = 1;
-        expect(encodeDecodeCaesar(text, caesarFlag)).toBe('bcde');
+        expect(encodeDecodeCaesar(text, caesarFlag)).toBe('aBcDebA1');
+    })
+
+    test('should correct decode english letters', () => {
+        const text = 'AbCda1';
+        const caesarFlag = 0;
+        expect(encodeDecodeCaesar(text, caesarFlag)).toBe('ZaBcz1');
     })
 })
 
@@ -34,8 +40,31 @@ describe('ROT-8 cipher', () => {
     })
 
     test('should correct encode english letters', () => {
-        const text = 'abcde';
+        const text = 'xYzAbc1';
         const ROT8Flag = 1;
-        expect(encodeDecodeROT8(text, ROT8Flag)).toBe('ijklm');
+        expect(encodeDecodeROT8(text, ROT8Flag)).toBe('fGhIjk1');
+    })
+
+    test('should correct decode english letters', () => {
+        const text = 'xYzaBc1';
+        const ROT8Flag = 0;
+        expect(encodeDecodeROT8(text, ROT8Flag)).toBe('pQrsTu1');
+    })
+})
+
+describe('Atbash cipher', () => {
+    test('should not encode numbers', () => {
+        const text = '12345';
+        expect(encodeDecodeAtbash(text)).toBe(text);
+    })
+
+    test('should not encode russian letters', () => {
+        const text = 'абвгд';
+        expect(encodeDecodeAtbash(text)).toBe(text);
+    })
+
+    test('should correct encode english letters', () => {
+        const text = 'abCDe';
+        expect(encodeDecodeAtbash(text)).toBe('zyXWv');
     })
 })
